8ffc28d6df798003709429f4c0f0cda35d61e40e
piroz <piroz@users.noreply.github.com>
2019-09-04 01:21:21 +0900

    add paper.js sample

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,76 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-paper-js
-      </h1>
-      <h2 class="subtitle">
-        paper js sample
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="col-7">
+      <p class="text">click path stroke</p>
+      <canvas id="editor"></canvas>
+    </div>
+    <div class="col-3">
+      <table class="table" style="width: 100%">
+        <tbody>
+          <tr>
+            <th>area</th>
+            <td>{{~~path.area}}</td>
+          </tr>
+          <tr>
+            <th>bounds</th>
+            <td>{{path.bounds}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import paper from "paper";
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      path: {}
+    };
+  },
+  created() {
+    paper.install(window);
+  },
+  mounted() {
+    paper.setup("editor");
+    this.startPaper();
+  },
+  methods: {
+    startPaper() {
+      paper.view.viewSize = [paper.view.size.width, window.innerHeight];
+      paper.activate();
+
+      let width = 100
+
+      let rectangle = new paper.Rectangle(
+        new paper.Point(paper.view.size.width / 2 - width / 2, window.innerHeight / 2 - width / 2),
+        new paper.Size(width, width)
+      );
+
+      this.path = new paper.Path.Rectangle(rectangle);
+      this.path.strokeColor = "black";
+      this.path.strokeWidth = 5;
+
+      paper.view.onClick = (e) => {
+        let hit = paper.project.hitTest(e.point);
+
+        if (hit && hit.item instanceof paper.Path) {
+          this.path.bounds.width = width * 2
+        } else {
+          this.path.bounds.width = width
+        }
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
 .container {
   margin: 0 auto;
+  width: 100%;
   min-height: 100vh;
   display: flex;
   justify-content: center;
@@ -48,25 +78,31 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+#editor {
+  width: 100%;
+  height: 100%;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.text {
+  position: absolute;
+  top: 30%;
+  left: 30%;
+  animation-name: my-fade-in;
+  animation-duration: 0.5s;
+  animation-delay: 0.5s;
+  animation-direction: alternate-reverse;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-out;
 }
 
-.links {
-  padding-top: 15px;
+@keyframes my-fade-in {
+  from {
+    opacity: 0;
+    transform: scale(1.2, 1.2)
+  }
+  to {
+    opacity: 1;
+    transform: scale(1, 1)
+  }
 }
 </style>
